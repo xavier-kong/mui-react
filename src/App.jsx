d9e932b42526398b9e9ad22bfb8857981b296b07
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-20 17:22:44 +0800

    added up to 17:13

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons';
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography>
+                            
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default App;
