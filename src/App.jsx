979bbd06dcc8ebee3628808d6271101dca4a887a
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-21 17:04:29 +0800

    added up to 21:35

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const App = () => {
     return (
@@ -20,11 +22,16 @@ const App = () => {
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
                         </Typography>
-                        <Typography>
-                            
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta laboriosam, odio hic nihil autem dolorem sit possimus voluptas ipsum ad consequatur, porro commodi ab quod eaque! Quasi, fugit illo.
                         </Typography>
                     </Container>
                 </div>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
             </main>
         </>
     )
